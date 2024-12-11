a1c23b2e91f54f7b6ea0f3f1302428dcff3a26c1
knght <knght.dev@pm.me>
2024-12-11 10:20:10 +0100

    tests and input loop

--- main.go
+++ main.go
@@ -1,9 +1,35 @@
-package main;
+package main
 
-import(
+import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 )
 
 func main(){
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		rawInput := scanner.Text()
+		input := cleanInput(rawInput)
+		if len(input) > 0 {
+			fmt.Printf("Your command was %s\n", input[0])
+		}
+	}
+}
+
+func cleanInput(text string) []string{
+	text = strings.ToLower(text)
+	text = strings.TrimSpace(text)
+	result := make([]string,0)	
+	for _, w := range strings.Fields(text){
+		if w == ""{
+			continue
+		}
+		result = append(result, w)
+	}
+
+	return result
 }
---
+++ repl_test.go
@@ -0,0 +1,40 @@
+package main
+
+import(
+	"testing"
+)
+
+func TestCleanInput(t *testing.T) {
+	cases := []struct {
+		input    string
+		expected []string
+	}{
+		{
+			input:    "  hello  world  ",
+			expected: []string{"hello", "world"},
+		},
+		{
+			input:    "  dev     woRld Go  ",
+			expected: []string{"dev", "world", "go"},
+		},
+		{
+			input:    "Charmander Bulbasaur PIKACHU",
+			expected: []string{"charmander", "bulbasaur", "pikachu"},
+		},
+	}
+
+	for _, c := range cases {
+		actual := cleanInput(c.input)
+		if len(actual) != len(c.expected){
+			t.Errorf("number of words does not match.\nExpected:\t%d\nActual\t%v\n", len(c.expected), len(actual))
+			
+		}
+		for i := range actual {
+			word := actual[i]
+			expectedWord := c.expected[i]
+			if word != expectedWord{
+				t.Errorf("word does not match:\nExpected:\t%v\nActual:\t%v\n", expectedWord, word)
+			}
+		}
+	}
+}
